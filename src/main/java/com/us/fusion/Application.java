44b13a8087a2a1056acc3dcd99a59fb77f62fc4d
杨伊博 <527515025@qq.com>
2018-12-27 17:33:48 +0800

    modify name

--- src/main/java/com/us/fusion/Application.java
+++ src/main/java/com/us/fusion/Application.java
@@ -1,14 +1,9 @@
-package com.us.fusion7;
+package com.us.fusion;
 
 import com.us.person.Person;
-import org.kie.api.KieBaseConfiguration;
 import org.kie.api.KieServices;
-import org.kie.api.conf.EventProcessingOption;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.rule.EntryPoint;
-
-import java.util.Scanner;
 
 /**
  * Created by yangyibo on 17/1/3.
@@ -23,24 +18,9 @@ public class Application {
     public static void run() {
 
         KieSession ks = getSession();
-//        // and start inserting your facts into the entry point
-//
         Person p1 = new Person("白展堂", 2);
         Person p2 = new Person("李大嘴", 16);
 
-        EntryPoint boyStream = ks.getEntryPoint("boy");
-//        boyStream.insert( p1 );
-//        boyStream.insert( p2 );
-//
-//
-//        EntryPoint  youthStream= ks.getEntryPoint( "youth" );
-//        youthStream.insert( p1 );
-//        youthStream.insert( p2 );
-
-//        EntryPoint  ageSumStream= ks.getEntryPoint( "ageSum" );
-//      for (int i=0;i<5;i++){
-//          ageSumStream.insert( p1 );
-//          ageSumStream.insert( p2 );
         ks.insert(p1);
         try {
             Thread.sleep(5000);
@@ -55,7 +35,6 @@ public class Application {
         System.out.println("总执行了" + count + "条规则------------------------------");
 
         ks.dispose();
-        System.out.println(p1.getAge()+"------------"+p2.getAge());
     }
 
 
