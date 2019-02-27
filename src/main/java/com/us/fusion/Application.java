6837dcb59d8f4644673b72dd5b143a8e1e3d05c0
杨伊博 <527515025@qq.com>
2019-02-27 13:42:14 +0800

    add drools fusion

--- src/main/java/com/us/fusion/Application.java
+++ src/main/java/com/us/fusion/Application.java
@@ -1,12 +1,15 @@
 package com.us.fusion;
 
-import com.us.person.Person;
+import com.us.model.Person;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import java.util.Date;
+
 /**
  * Created by yangyibo on 17/1/3.
+ * @author yangyibo
  */
 public class Application {
     private static KieSession getSession() {
@@ -18,18 +21,18 @@ public class Application {
     public static void run() {
 
         KieSession ks = getSession();
-        Person p1 = new Person("白展堂", 2);
-        Person p2 = new Person("李大嘴", 16);
-
-        ks.insert(p1);
+        Person p1 = new Person("白展堂", 52,new Date());
+        Person p2 = new Person("佟湘玉", 57,new Date());
         try {
-            Thread.sleep(5000);
+            Thread.sleep(4000);
         } catch (InterruptedException e) {
             System.out.println(e);
         }
-        ks.insert(p2);
-//      }
+        Person p3 = new Person("李大嘴", 56,new Date());
 
+        ks.insert(p1);
+        ks.insert(p2);
+        ks.insert(p3);
 
         int count = ks.fireAllRules();
         System.out.println("总执行了" + count + "条规则------------------------------");
